ae3101eb5135664513699f1db1e39db1c5228308
JECGEdu <122706533+JoeGCode@users.noreply.github.com>
2025-03-29 14:15:11 +0000

    Confirm email page

---
+++ src/app/auth/check-email/page.tsx
@@ -0,0 +1,12 @@
+function CheckEmailPage() {
+  return (
+    <div className="flex h-full flex-1 flex-col items-center justify-center p-4 text-center">
+      <h1 className="text-4xl">Thank you for signing up</h1>
+      <p className="text-2xl">
+        Please confirm your email by checking your inbox and following the link
+      </p>
+    </div>
+  );
+}
+
+export default CheckEmailPage;
